04ee43b7ff1dd46c8a9e0c089a53f412c0f4a813
Sarawoot <sarawoot.piyaarayanunt@gmail.com>
2016-05-15 09:12:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+const math = require('mathjs');
+const SDK = require('inline-sdk');
+const CommandType = SDK.CommandType;
+
+const inline = new SDK();
+
+inline.onCommand((type, payload) => {
+  if (type !== CommandType.MESSAGE) return;
+	const text = payload.args.join(' ');
+	const result = math.eval(text);
+	if (result) {
+		inline.sendText(result);
+	}
+});
+
+console.log("test");
